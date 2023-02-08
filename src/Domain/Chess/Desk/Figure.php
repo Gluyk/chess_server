59f64dfb401cb577bf0115cde825f78056bb30cb
Nikolay Tyurin <t608833175@gmail.com>
2023-02-08 16:39:19 +0100

    add enums: Figure, Files, Ranks

---
+++ src/Domain/Chess/Desk/Figure.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Domain\Chess\Desk;
+
+enum Figure: string
+{
+    case Knight = 'knight';
+}
---
+++ src/Domain/Chess/Desk/Ranks.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Domain\Chess\Desk;
+
+enum Ranks: string
+{
+    case A = 'a';
+    case B = 'b';
+    case C = 'c';
+    case D = 'd';
+    case E = 'e';
+    case F = 'f';
+    case G = 'g';
+    case H = 'h';
+}
