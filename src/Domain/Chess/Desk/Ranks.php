dd3046dda7e790e89a742fb8f0bbc17c48255f53
Nikolay Tyurin <t608833175@gmail.com>
2023-02-09 14:43:08 +0100

    Add getAsNumber and getFromNumber to Ranks enum

--- src/Domain/Chess/Desk/Ranks.php
+++ src/Domain/Chess/Desk/Ranks.php
@@ -12,4 +12,23 @@ enum Ranks: string
     case F = 'f';
     case G = 'g';
     case H = 'h';
+
+    public function getAsNumber(): int
+    {
+        return ord($this->value) - 96;
+    }
+
+    public static function getFromNumber(int $position): Ranks
+    {
+        return match ($position) {
+            1 => self::A,
+            2 => self::B,
+            3 => self::C,
+            4 => self::D,
+            5 => self::E,
+            6 => self::F,
+            7 => self::G,
+            8 => self::H,
+        };
+    }
 }
